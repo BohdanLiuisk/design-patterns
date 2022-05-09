5bc0fe00407c675903a6dd2478d232c964ced8a8
b.liusik <66973922+BohdanLiuisk@users.noreply.github.com>
2022-05-09 03:03:31 +0300

    add decorator cycle policy

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DecoratorPattern
+﻿using System.Text;
+
+namespace DecoratorPattern
 {
     public interface IShape
     {
@@ -29,19 +31,57 @@
         public string AsString() => $"Circle with radius { _radius }";
     }
 
-    public class ColoredShape : IShape
+    public abstract class ShapeDecorator : IShape
+    {
+        protected internal readonly List<Type> Types = new();
+
+        protected internal readonly IShape Shape;
+
+        public ShapeDecorator(IShape shape)
+        {
+            Shape = shape ?? throw new ArgumentException(null, nameof(shape));
+            if(shape is ShapeDecorator shapeDecorator)
+            {
+                Types.AddRange(shapeDecorator.Types);
+            }
+        }
+
+        public abstract string AsString();
+    }
+
+    public abstract class ShapeDecorator<TSelf, TCyclePolicy> : ShapeDecorator
+        where TCyclePolicy : ShapeDecoratorCyclePolicy, new()
+    {
+        protected TCyclePolicy CyclePolicy = new();
+
+        protected ShapeDecorator(IShape shape) : base(shape)
+        {
+            if (CyclePolicy.TypeAdditionAllowed(typeof(TSelf), Types))
+            {
+                Types.Add(typeof(TSelf));
+            }
+        }
+    }
+
+    public class ColoredShape : ShapeDecorator<ColoredShape, ThrowOnCyclePolicy>
     {
         private readonly string _color;
 
-        private readonly IShape _decorated;
-
-        public ColoredShape(IShape decorated, string color) 
+        public ColoredShape(IShape decorated, string color) : base(decorated) 
         {
-            _decorated = decorated ?? throw new ArgumentException(null, nameof(decorated));
             _color = color ?? throw new ArgumentException(null, nameof(color));
         }
 
-        public string AsString() => $"{ _decorated.AsString() } has color {_color}";
+        public override string AsString() 
+        {
+            var stringBuilder = new StringBuilder()
+                .Append($"{ Shape.AsString() }");
+            if (CyclePolicy.ApplicationAllowed(Types[0], Types.Skip(1).ToList()))
+            {
+                stringBuilder.Append($" has the color { _color }");
+            }
+            return stringBuilder.ToString();
+        }
     }
 
     public class TransparentShape : IShape
@@ -59,6 +99,48 @@
         public string AsString() => $"{ _decorated.AsString() } with transparency { _transparency }%";
     }
 
+    public abstract class ShapeDecoratorCyclePolicy
+    {
+        public abstract bool TypeAdditionAllowed(Type type, IList<Type> allTypes);
+
+        public abstract bool ApplicationAllowed(Type type, IList<Type> allTypes);
+    }
+
+    public class CyclesAllowedPolicy : ShapeDecoratorCyclePolicy
+    {
+        public override bool ApplicationAllowed(Type type, IList<Type> allTypes)
+        {
+            return true;
+        }
+
+        public override bool TypeAdditionAllowed(Type type, IList<Type> allTypes)
+        {
+            return true;
+        }
+    }
+
+    public class ThrowOnCyclePolicy : ShapeDecoratorCyclePolicy
+    {
+        private bool Handle(Type type, IList<Type> allTypes)
+        {
+            if (allTypes.Contains(type))
+            {
+                throw new InvalidOperationException($"Cycle detected! Type is already { type.FullName }");
+            }
+            return true;
+        }
+
+        public override bool ApplicationAllowed(Type type, IList<Type> allTypes)
+        {
+            return Handle(type, allTypes);
+        }
+
+        public override bool TypeAdditionAllowed(Type type, IList<Type> allTypes)
+        {
+            return Handle(type, allTypes);
+        }
+    }
+
     static class Program
     {
         static void Main(string[] args)
@@ -69,6 +151,9 @@
             Console.WriteLine(circle.AsString());
             var coloredSqurare = new ColoredShape(square, "red");
             Console.WriteLine(coloredSqurare.AsString());
+            var coloredSqurare2 = new ColoredShape(coloredSqurare, "green");
+            Console.WriteLine(coloredSqurare2.AsString());
+
             var transparentShape = new TransparentShape(coloredSqurare, 20.5);
             Console.WriteLine(transparentShape.AsString());
         }
